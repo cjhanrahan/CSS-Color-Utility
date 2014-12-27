e0451e2d9fa0b5e5de5fadd006eb7a5a6f2e733c
Conor Hanrahan <conor.hanrahan@conorhanrahan.com>
2014-12-26 18:07:50 -0800

    Added all necessary tags, about to rework logic

--- app.js
+++ app.js
@@ -2,54 +2,30 @@ window.onload = function(){
   
   window.$ = {
 
-  
-    start: function() {
-      $.updateReadouts();
 
-      _.objectForEach($.inputs, function(input){
-        input.sliderNode.addEventListener('input', function(){
-          $.updateReadouts();
-          $.updateExampleText();
-        });
+ 
+    start: function() {
+
+    
+    },
+
+    setupInputHandlers: function(){
+      _.nodeForEach('.backgroundForegroundSections', function(section){
+        
       });
     },
 
+
+
     updateReadouts: function() {
-      _.objectForEach($.inputs, function(input){
-        input.readoutNode.innerHTML = input.sliderNode.value + input.symbol;
-      });
+
     },
 
     updateExampleText: function() {
-      var color = "hsl(" + $.getInputValue("hue") +
-        "," + $.getInputValue("saturation") + "%" +
-        "," + $.getInputValue("lightness") + "%)"; 
-      var exampleNode = document.querySelector("p");
 
-      exampleNode.style.backgroundColor = color;
     },
 
-    getInputValue: function(inputType) {
-      return $.inputs[inputType].sliderNode.value;
-    },
 
-    inputs: {
-      hue: {
-        sliderNode: document.querySelector("input.hue"),
-        readoutNode: document.querySelector(".readout.hue"),
-        symbol: '\u00b0'
-      },
-      saturation: {
-        sliderNode: document.querySelector("input.saturation"),
-        readoutNode: document.querySelector(".readout.saturation"),
-        symbol: '%'
-      },
-      lightness: {
-        sliderNode: document.querySelector("input.lightness"),
-        readoutNode: document.querySelector(".readout.lightness"),
-        symbol: '%'
-      }
-    }
   };
 
   $.start();
