aee02e169ec53c6baee571033d2970549c93319a
Conor Hanrahan <conor.hanrahan@ayasdi.com>
2015-04-20 23:00:07 -0700

    Added rgb validation

--- dev/scripts/validate.js
+++ dev/scripts/validate.js
@@ -21,6 +21,45 @@ define(function () {
         }
 
         return {isValid: true, reason: null};
+    },
+
+    red:        function (numberString) { return this.integerBetween(0, 255)(numberString); },
+    green:      function (numberString) { return this.integerBetween(0, 255)(numberString); },
+    blue:       function (numberString) { return this.integerBetween(0, 255)(numberString); },
+    hue:        function (numberString) { return this.integerBetween(0, 360)(numberString); },
+    saturation: function (numberString) { return this.integerBetween(0, 100)(numberString); },
+    lightness:  function (numberString) { return this.integerBetween(0, 100)(numberString); },
+
+
+    integerBetween: function (min, max) {
+        return function (numberString) {
+            var number = Number(numberString);
+            if (isNaN(number)) {
+                return {
+                    isValid: false,
+                    reason: 'Value provided is not a number'
+                };
+            }
+            else if (number < min) { 
+                return {
+                    isValid: false,
+                    reason: 'Value provided cannot be below ' + min
+                };
+            }
+            else if (number > max) { 
+                return {
+                    isValid: false,
+                    reason: 'Value provided cannot be above ' + max
+                };
+            }
+            else if (number - Math.round(number) !== 0) {
+                return {
+                    isValid: false,
+                    reason: 'Value provided must be an integer (app limitiation)'
+                };
+            }
+            return {isValid: true, reason: null};
+        };
     }
   };
 
