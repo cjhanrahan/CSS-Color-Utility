b4c5998691ac3a873b0522b7a236ddff0e9985a4
Conor Hanrahan <conor.hanrahan@ayasdi.com>
2015-07-14 10:37:54 -0700

    Build page

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+requirejs.config({
+  baseUrl: 'scripts',
+
+  paths: {
+    'jasmine': '../lib/jasmine-2.1.3/jasmine',
+    'jasmine-html': '../lib/jasmine-2.1.3/jasmine-html',
+    'custom-boot': '../lib/jasmine-2.1.3/custom-boot',
+    'tests': '../tests',
+    'text': '../lib/text'
+  },
+
+  shim: {
+    'jasmine': {
+      exports: 'window.jasmineRequire',
+    },
+      'jasmine-html': {
+      'deps': ['jasmine'],
+      'exports': 'window.jasmineRequire'
+    },
+    'custom-boot': {
+      'deps': ['jasmine', 'jasmine-html'],
+      'exports': 'window.jasmineRequire'
+    }
+  }
+});
+
+
+
+var testSuites = [
+  'tests/cssPropertySuite',
+  'tests/convertSuite',
+  'tests/colorStateSuite',
+  'tests/validateSuite'
+];
+
+require(['app'], function (app) {
+    app.setUpApp();
+});
+
